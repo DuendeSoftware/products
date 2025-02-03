4b882b76433beb0a043dc3513133153c1a3fdf2a
Erwin van der Valk <erwin@vandervalk.pro>
2025-02-03 13:49:17 +0100

    tests now use a constant

--- bff/samples/Hosts.Tests/TestInfra/AppHostFixture.cs
+++ bff/samples/Hosts.Tests/TestInfra/AppHostFixture.cs
@@ -1,4 +1,5 @@
 using Aspire.Hosting;
+using Hosts.ServiceDefaults;
 using Microsoft.Extensions.Logging;
 
 #if !DEBUG_NCRUNCH
@@ -81,33 +82,16 @@ public class AppHostFixture : IAsyncLifetime
         await (await appHost.BuildAsync()).StartAsync();
 
         // Wait for all the services so that their logs are mostly written. 
-        await resourceNotificationService.WaitForResourceAsync(
-                "bff",
-                KnownResourceStates.Running
-            )
-            .WaitAsync(TimeSpan.FromSeconds(30));
 
-        await resourceNotificationService.WaitForResourceAsync(
-                "bff-ef",
-                KnownResourceStates.Running
-            )
-            .WaitAsync(TimeSpan.FromSeconds(30));
+        foreach (var resource in AppHostServices.All)
+        {
+            await resourceNotificationService.WaitForResourceAsync(
+                    resource,
+                    KnownResourceStates.Running
+                )
+                .WaitAsync(TimeSpan.FromSeconds(30));
+        }
 
-        await resourceNotificationService.WaitForResourceAsync(
-                "bff-webassembly-per-component",
-                KnownResourceStates.Running
-            )
-            .WaitAsync(TimeSpan.FromSeconds(30));
-        await resourceNotificationService.WaitForResourceAsync(
-                "bff-dpop",
-                KnownResourceStates.Running
-            )
-            .WaitAsync(TimeSpan.FromSeconds(30));
-        await resourceNotificationService.WaitForResourceAsync(
-                "migrations",
-                KnownResourceStates.Running
-            )
-            .WaitAsync(TimeSpan.FromSeconds(30));
 
 #endif //#DEBUG_NCRUNCH
     }
