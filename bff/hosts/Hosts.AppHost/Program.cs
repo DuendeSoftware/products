e1c6b40b1ae3b54108d35fc4b4c145eafa85a5ee
Erwin van der Valk <erwin@vandervalk.pro>
2025-02-27 11:43:41 +0100

    aspire works

--- bff/hosts/Hosts.AppHost/Program.cs
+++ bff/hosts/Hosts.AppHost/Program.cs
@@ -60,8 +60,10 @@ builder.AddProject<BffLocalApi>("templates-bff-local", launchProfileName: null)
 builder.AddProject<BffRemoteApi>("templates-bff-remote", launchProfileName: null)
     .WithHttpsEndpoint(5310, name: "bff-remote");
 
-builder.AddProject<BffBlazor>("templates-bff-blazor", launchProfileName: null)
-    .WithHttpsEndpoint(5320, name: "bff-blazor");
+builder.AddProject<BffBlazor>("templates-bff-blazor")
+    
+    //.WithHttpsEndpoint(5320, name: "bff-blazor")
+    ;
 
 builder.Build().Run();
 
