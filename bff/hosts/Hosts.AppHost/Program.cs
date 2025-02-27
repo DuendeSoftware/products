a3efa6924ce1c12dc51ded8a19310c5be8b96c96
Erwin van der Valk <erwin@vandervalk.pro>
2025-02-27 10:21:01 +0100

    adding blazor templates

--- bff/hosts/Hosts.AppHost/Program.cs
+++ bff/hosts/Hosts.AppHost/Program.cs
@@ -60,6 +60,9 @@ builder.AddProject<BffLocalApi>("templates-bff-local", launchProfileName: null)
 builder.AddProject<BffRemoteApi>("templates-bff-remote", launchProfileName: null)
     .WithHttpsEndpoint(5310, name: "bff-remote");
 
+builder.AddProject<BffBlazor>("templates-bff-blazor", launchProfileName: null)
+    .WithHttpsEndpoint(5320, name: "bff-blazor");
+
 builder.Build().Run();
 
 public static class Extensions
---
+++ bff/templates/src/BffBlazor/BffBlazor/Program.cs
@@ -0,0 +1,81 @@
+using BffBlazor;
+using BffBlazor.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBff();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "cookie";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("cookie", options =>
+    {
+        options.Cookie.Name = "__Host-blazor";
+        options.Cookie.SameSite = SameSiteMode.Strict;
+    })
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://demo.duendesoftware.com";
+        options.ClientId = "interactive.confidential";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        options.ResponseMode = "query";
+
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.SaveTokens = true;
+        options.MapInboundClaims = false;
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("api");
+        options.Scope.Add("offline_access");
+
+        options.TokenValidationParameters.NameClaimType = "name";
+        options.TokenValidationParameters.RoleClaimType = "role";
+    });
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseBff();
+app.UseAuthorization();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BffBlazor.Client._Imports).Assembly);
+
+app.MapBffManagementEndpoints();
+WeatherEndpoints.Map(app);
+
+app.Run();
