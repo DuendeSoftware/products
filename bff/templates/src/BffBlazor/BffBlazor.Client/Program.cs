83702ad97fc7197a025ec959192600c8dfcc9495
Erwin van der Valk <erwin@vandervalk.pro>
2025-02-27 11:51:49 +0100

    wip

--- bff/templates/src/BffBlazor/BffBlazor.Client/Program.cs
+++ bff/templates/src/BffBlazor/BffBlazor.Client/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Duende.Bff.Blazor.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+    .AddBffBlazorClient() // Provides auth state provider that polls the /bff/user endpoint
+    .AddCascadingAuthenticationState();
+
+builder.Services.AddLocalApiHttpClient<WeatherHttpClient>();
 
 await builder.Build().RunAsync();
