2ddf8e337db073dd1c1bee43998d337926f6cce8
Erwin van der Valk <erwin@vandervalk.pro>
2025-03-05 10:18:32 +0100

    testing blazor seems to work

---
+++ bff/test/Bff.Blazor.UnitTests/BffBlazorTests.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using Duende.Bff;
+using Duende.Bff.Tests.TestHosts;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using Xunit.Abstractions;
+
+namespace Bff.Blazor.UnitTests
+{
+    public class BffBlazorTests : OutputWritingTestBase
+    {
+        protected readonly IdentityServerHost IdentityServerHost;
+        protected ApiHost ApiHost;
+        protected BffBlazorHost BffHost;
+
+        public BffBlazorTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            IdentityServerHost = new IdentityServerHost(WriteLine);
+            ApiHost = new ApiHost(WriteLine, IdentityServerHost, "scope1");
+
+            BffHost = new BffBlazorHost(WriteLine, IdentityServerHost, ApiHost, "spa");
+
+            IdentityServerHost.Clients.Add(new Client
+            {
+                ClientId = "spa",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RedirectUris = { "https://app/signin-oidc" },
+                PostLogoutRedirectUris = { "https://app/signout-callback-oidc" },
+                BackChannelLogoutUri = "https://app/bff/backchannel",
+                AllowOfflineAccess = true,
+                AllowedScopes = { "openid", "profile", "scope1" }
+            });
+
+
+
+            IdentityServerHost.OnConfigureServices += services => {
+                services.AddTransient<IBackChannelLogoutHttpClient>(provider =>
+                    new DefaultBackChannelLogoutHttpClient(
+                        BffHost!.HttpClient,
+                        provider.GetRequiredService<ILoggerFactory>(),
+                        provider.GetRequiredService<ICancellationTokenProvider>()));
+
+                services.AddSingleton<DefaultAccessTokenRetriever>();
+            };
+        }
+
+        [Fact]
+        public async Task Can_get_home()
+        {
+            var response = await BffHost.HttpClient.GetAsync("/");
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+
+        public override async Task InitializeAsync()
+        {
+            await IdentityServerHost.InitializeAsync();
+            await ApiHost.InitializeAsync();
+            await BffHost.InitializeAsync();
+            await base.InitializeAsync();
+        }
+
+        public override async Task DisposeAsync()
+        {
+            await ApiHost.DisposeAsync();
+            await BffHost.DisposeAsync();
+            await IdentityServerHost.DisposeAsync();
+            await base.DisposeAsync();
+
+        }
+    }
+}
--- bff/test/Bff.Tests/TestFramework/TestBrowserClient.cs
+++ bff/test/Bff.Tests/TestFramework/TestBrowserClient.cs
@@ -5,6 +5,7 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
