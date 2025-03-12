c86bdaf5f0e732c6fa8e2784bb91b7f312b5a22b
Erwin van der Valk <erwin@vandervalk.pro>
2025-03-12 14:52:38 +0100

    transform logout url in transformer

--- bff/hosts/Blazor/PerComponent/Hosts.Bff.Blazor.PerComponent/Program.cs
+++ bff/hosts/Blazor/PerComponent/Hosts.Bff.Blazor.PerComponent/Program.cs
@@ -42,6 +42,10 @@ builder.Services.AddAuthentication(options =>
     {
         options.Cookie.Name = "__Host-blazor";
         options.Cookie.SameSite = SameSiteMode.Strict;
+        options.Events.OnValidatePrincipal += context =>
+        {
+            return Task.CompletedTask;
+        };
     })
     .AddOpenIdConnect("oidc", options =>
     {
