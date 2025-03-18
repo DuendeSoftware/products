b8df3aaff1463382ff44478f7897ff7231bf99a6
Stu Frankish <stuart.frankish@duendesoftware.com>
2025-03-18 14:01:20 +0000

    Moved additional calls away from constants to use configuration instead

--- identity-server/clients/src/ConsoleCibaClient/Program.cs
+++ identity-server/clients/src/ConsoleCibaClient/Program.cs
@@ -25,6 +25,7 @@ builder.Services.AddHttpClient("SimpleApi", client =>
 
 // Resolve the authority from the configuration.
 var authority = builder.Configuration["is-host"];
+var simpleApi = builder.Configuration["simple-api"];
 
 IDiscoveryCache _cache = new DiscoveryCache(authority);
 
