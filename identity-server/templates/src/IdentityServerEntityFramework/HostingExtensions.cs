f472a8ec4ffc090f156d118c259bc1d6369b04bc
Maarten Balliauw <maarten@maartenballiauw.be>
2025-05-12 11:48:22 +0200

    Microsoft.AspnetCore.Authentication.Google -> Google.Apis.Auth.AspNetCore3

--- identity-server/templates/src/IdentityServerEntityFramework/HostingExtensions.cs
+++ identity-server/templates/src/IdentityServerEntityFramework/HostingExtensions.cs
@@ -1,4 +1,5 @@
 using Duende.IdentityServer;
+using Google.Apis.Auth.AspNetCore3;
 using IdentityServerHost;
 using IdentityServerHost.Pages.Admin.ApiScopes;
 using IdentityServerHost.Pages.Admin.Clients;
@@ -44,16 +45,21 @@ internal static class HostingExtensions
             .AddLicenseSummary();
 
         builder.Services.AddAuthentication()
-            .AddGoogle(options =>
-            {
-                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
-
-                // register your IdentityServer with Google at https://console.developers.google.com
-                // enable the Google+ API
-                // set the redirect URI to https://localhost:5001/signin-google
-                options.ClientId = "copy client ID from Google here";
-                options.ClientSecret = "copy client secret from Google here";
-            });
+            .AddGoogleOpenIdConnect(
+                authenticationScheme: GoogleOpenIdConnectDefaults.AuthenticationScheme,
+                displayName: "Google",
+                configureOptions: options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+  
+                    // register your IdentityServer with Google at https://console.developers.google.com
+                    // enable the Google+ API
+                    // set the redirect URI to https://localhost:5001/signin-google
+                    options.ClientId = "copy client ID from Google here";
+                    options.ClientSecret = "copy client secret from Google here";
+          
+                    options.CallbackPath = "/signin-google";
+                });
 
 
         // this adds the necessary config for the simple admin/config pages
