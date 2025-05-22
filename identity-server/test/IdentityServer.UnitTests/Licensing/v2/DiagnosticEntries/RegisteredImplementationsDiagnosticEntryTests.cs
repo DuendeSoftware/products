b00567370d49715152679024be60db95d8c19b20
Brett Hazen <2651260+bhazen@users.noreply.github.com>
2025-05-22 08:25:55 -0500

    Re-worked registered implementations diagnostic to be more dynamic and group by namespace

--- identity-server/test/IdentityServer.UnitTests/Licensing/v2/DiagnosticEntries/RegisteredImplementationsDiagnosticEntryTests.cs
+++ identity-server/test/IdentityServer.UnitTests/Licensing/v2/DiagnosticEntries/RegisteredImplementationsDiagnosticEntryTests.cs
@@ -3,6 +3,7 @@
 
 using Duende.IdentityServer.Licensing.V2.Diagnostics.DiagnosticEntries;
 using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Stores;
 using Microsoft.Extensions.DependencyInjection;
 using UnitTests.Common;
 
@@ -11,7 +12,7 @@ namespace IdentityServer.UnitTests.Licensing.V2.DiagnosticEntries;
 public class RegisteredImplementationsDiagnosticEntryTests
 {
     [Fact]
-    public async Task WriteAsync_ShouldWriteRegisteredServicesInfo()
+    public async Task WriteAsync_ShouldWriteRegisteredImplementationInfo()
     {
         var serviceProvider = new ServiceCollection()
             .AddSingleton<IProfileService, MockProfileService>()
@@ -20,14 +21,30 @@ public class RegisteredImplementationsDiagnosticEntryTests
 
         var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
 
-        var registeredServices = result.RootElement.GetProperty("RegisteredServices");
-        var services = registeredServices.GetProperty("Services");
-        var firstEntry = services.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
-        var assemblyInfo = firstEntry.GetProperty(nameof(IProfileService));
+        var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
+        var namespaceEntry = registeredImplementations.GetProperty(typeof(IProfileService).Namespace!);
+        var profileServiceEntry = namespaceEntry.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
+        var assemblyInfo = profileServiceEntry.GetProperty(nameof(IProfileService)).EnumerateArray().First();
         var expectedTypeInfo = typeof(MockProfileService);
         assemblyInfo.GetProperty("TypeName").GetString().ShouldBe(expectedTypeInfo.FullName);
         assemblyInfo.GetProperty("Assembly").GetString().ShouldBe(expectedTypeInfo.Assembly.GetName().Name);
         assemblyInfo.GetProperty("AssemblyVersion").GetString().ShouldBe(expectedTypeInfo.Assembly.GetName().Version?.ToString());
+    }
+
+    [Fact]
+    public async Task WriteAsync_GroupsImplementationsByNamespace()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IProfileService, MockProfileService>()
+            .AddSingleton<IClientStore, InMemoryClientStore>()
+            .BuildServiceProvider();
+        var subject = new RegisteredImplementationsDiagnosticEntry(serviceProvider);
+
+        var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
+
+        var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
+        registeredImplementations.TryGetProperty(typeof(IProfileService).Namespace!, out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty(typeof(IClientStore).Namespace!, out _).ShouldBeTrue();
     }
 
     [Fact]
@@ -37,10 +54,10 @@ public class RegisteredImplementationsDiagnosticEntryTests
 
         var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
 
-        var registeredServices = result.RootElement.GetProperty("RegisteredServices");
-        var services = registeredServices.GetProperty("Services");
-        var firstEntry = services.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
-        var assemblyInfo = firstEntry.GetProperty(nameof(IProfileService));
+        var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
+        var namespaceEntry = registeredImplementations.GetProperty(typeof(IProfileService).Namespace!);
+        var profileServiceEntry = namespaceEntry.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
+        var assemblyInfo = profileServiceEntry.GetProperty(nameof(IProfileService)).EnumerateArray().First();
         assemblyInfo.GetProperty("TypeName").GetString().ShouldBe("Not Registered");
         assemblyInfo.GetProperty("Assembly").GetString().ShouldBe("Not Registered");
         assemblyInfo.GetProperty("AssemblyVersion").GetString().ShouldBe("Not Registered");
