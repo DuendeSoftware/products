7fac54572f66919e072571cc45f169a87fab611d
Brett Hazen <2651260+bhazen@users.noreply.github.com>
2025-05-30 14:14:02 -0500

    Moved away from generics to drive registered implementation diagnostic entry to maintain AOT compatability

--- identity-server/src/IdentityServer/Licensing/V2/Diagnostics/DiagnosticEntries/RegisteredImplementationsDiagnosticEntry.cs
+++ identity-server/src/IdentityServer/Licensing/V2/Diagnostics/DiagnosticEntries/RegisteredImplementationsDiagnosticEntry.cs
@@ -2,40 +2,166 @@
 // See LICENSE in the project root for license information.
 
 using System.Text.Json;
+using Duende.IdentityServer.Hosting;
 using Duende.IdentityServer.Internal;
+using Duende.IdentityServer.ResponseHandling;
 using Duende.IdentityServer.Services;
+using Duende.IdentityServer.Services.KeyManagement;
 using Duende.IdentityServer.Stores;
-using Microsoft.Extensions.Logging;
+using Duende.IdentityServer.Stores.Serialization;
+using Duende.IdentityServer.Validation;
 
 namespace Duende.IdentityServer.Licensing.V2.Diagnostics.DiagnosticEntries;
 
-internal class RegisteredImplementationsDiagnosticEntry : IDiagnosticEntry
+internal class RegisteredImplementationsDiagnosticEntry(ServiceCollectionAccessor serviceCollectionAccessor)
+    : IDiagnosticEntry
 {
-    private readonly ServiceCollectionAccessor _serviceCollectionAccessor;
-    private readonly ILogger<RegisteredImplementationsDiagnosticEntry> _logger;
-    private readonly Dictionary<string, IEnumerable<Type>> _typesToInspect;
-
-    public RegisteredImplementationsDiagnosticEntry(ServiceCollectionAccessor serviceCollectionAccessor, ILogger<RegisteredImplementationsDiagnosticEntry> logger)
+    private readonly Dictionary<string, IEnumerable<Type>> _typesToInspect = new()
     {
-        _serviceCollectionAccessor = serviceCollectionAccessor;
-        _logger = logger;
-
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        _typesToInspect = assemblies.SelectMany(assembly => assembly.GetExportedTypes())
-            .Where(type => type.IsInterface && type.IsPublic && !type.IsGenericTypeDefinition && type.Namespace != null && type.Namespace.StartsWith("Duende.IdentityServer"))
-            .GroupBy(type => type.Namespace, type => type)
-            .ToDictionary(group => group.Key, group => group.OrderBy(type => type.Name).AsEnumerable());
-    }
+        {
+            "Root", [ typeof(IIdentityServerTools) ]
+        },
+        {
+            "Hosting", [
+                typeof(IEndpointHandler),
+                typeof(IEndpointResult),
+                typeof(IEndpointRouter),
+                typeof(IHttpResponseWriter<>)
+            ]
+        },
+        {
+            "Infrastructure", [typeof(IClock), typeof(IConcurrencyLock<>)]
+        },
+        {
+            "ResponseHandling", [
+                typeof(IAuthorizeInteractionResponseGenerator),
+                typeof(IAuthorizeResponseGenerator),
+                typeof(IBackchannelAuthenticationResponseGenerator),
+                typeof(IDeviceAuthorizationResponseGenerator),
+                typeof(IDiscoveryResponseGenerator),
+                typeof(IIntrospectionResponseGenerator),
+                typeof(IPushedAuthorizationResponseGenerator),
+                typeof(ITokenResponseGenerator),
+                typeof(ITokenRevocationResponseGenerator),
+                typeof(IUserInfoResponseGenerator)
+            ]
+        },
+        {
+            "Services", [
+                typeof(IAutomaticKeyManagerKeyStore),
+                typeof(IBackchannelAuthenticationInteractionService),
+                typeof(IBackchannelAuthenticationThrottlingService),
+                typeof(IBackchannelAuthenticationUserNotificationService),
+                typeof(IBackChannelLogoutHttpClient),
+                typeof(IBackChannelLogoutService),
+                typeof(ICache<>),
+                typeof(ICancellationTokenProvider),
+                typeof(IClaimsService),
+                typeof(IConsentService),
+                typeof(ICorsPolicyService),
+                typeof(IDeviceFlowCodeService),
+                typeof(IDeviceFlowInteractionService),
+                typeof(IDeviceFlowThrottlingService),
+                typeof(IEventService),
+                typeof(IEventSink),
+                typeof(IHandleGenerationService),
+                typeof(IIdentityServerInteractionService),
+                typeof(IIssuerNameService),
+                typeof(IJwtRequestUriHttpClient),
+                typeof(IKeyManager),
+                typeof(IKeyMaterialService),
+                typeof(ILogoutNotificationService),
+                typeof(IPersistedGrantService),
+                typeof(IProfileService),
+                typeof(IPushedAuthorizationSerializer),
+                typeof(IPushedAuthorizationService),
+                typeof(IRefreshTokenService),
+                typeof(IReplayCache),
+                typeof(IReturnUrlParser),
+                typeof(IServerUrls),
+                typeof(ISessionCoordinationService),
+                typeof(ISessionManagementService),
+                typeof(ISigningKeyProtector),
+                typeof(ISigningKeyStoreCache),
+                typeof(ITokenCreationService),
+                typeof(ITokenService),
+                typeof(IUserCodeGenerator),
+                typeof(IUserCodeService),
+                typeof(IUserSession)
+            ]
+        },
+        {
+            "Stores", [
+                typeof(IAuthorizationCodeStore),
+                typeof(IAuthorizationParametersMessageStore),
+                typeof(IBackChannelAuthenticationRequestStore),
+                typeof(IClientStore),
+                typeof(IConsentMessageStore),
+                typeof(IDeviceFlowStore),
+                typeof(IIdentityProviderStore),
+                typeof(IMessageStore<>),
+                typeof(IPersistentGrantSerializer),
+                typeof(IPersistedGrantStore),
+                typeof(IPushedAuthorizationRequestStore),
+                typeof(IReferenceTokenStore),
+                typeof(IRefreshTokenStore),
+                typeof(IResourceStore),
+                typeof(IServerSideSessionsMarker),
+                typeof(IServerSideSessionStore),
+                typeof(IServerSideTicketStore),
+                typeof(ISigningCredentialStore),
+                typeof(ISigningKeyStore),
+                typeof(IUserConsentStore),
+                typeof(IValidationKeysStore)
+            ]
+        },
+        {
+            "Validation", [
+                typeof(IApiSecretValidator),
+                typeof(IAuthorizeRequestValidator),
+                typeof(IBackchannelAuthenticationRequestIdValidator),
+                typeof(IBackchannelAuthenticationRequestValidator),
+                typeof(IBackchannelAuthenticationUserValidator),
+                typeof(IClientConfigurationValidator),
+                typeof(IClientSecretValidator),
+                typeof(ICustomAuthorizeRequestValidator),
+                typeof(ICustomBackchannelAuthenticationValidator),
+                typeof(ICustomTokenRequestValidator),
+                typeof(ICustomTokenValidator),
+                typeof(IDeviceAuthorizationRequestValidator),
+                typeof(IDeviceCodeValidator),
+                typeof(IDPoPProofValidator),
+                typeof(IEndSessionRequestValidator),
+                typeof(IExtensionGrantValidator),
+                typeof(IIdentityProviderConfigurationValidator),
+                typeof(IIntrospectionRequestValidator),
+                typeof(IJwtRequestValidator),
+                typeof(IPushedAuthorizationRequestValidator),
+                typeof(IRedirectUriValidator),
+                typeof(IResourceOwnerPasswordValidator),
+                typeof(IResourceValidator),
+                typeof(IScopeParser),
+                typeof(ISecretParser),
+                typeof(ISecretsListParser),
+                typeof(ISecretsListValidator),
+                typeof(ISecretValidator),
+                typeof(ITokenRequestValidator),
+                typeof(ITokenRevocationRequestValidator),
+                typeof(ITokenValidator),
+                typeof(IUserInfoRequestValidator)
+            ]
+        }
+    };
 
     public Task WriteAsync(Utf8JsonWriter writer)
     {
         writer.WriteStartObject("RegisteredImplementations");
 
-        foreach (var namespaceGroup in _typesToInspect)
+        foreach (var group in _typesToInspect)
         {
-            writer.WriteStartArray(namespaceGroup.Key);
+            writer.WriteStartArray(group.Key);
 
-            foreach (var type in namespaceGroup.Value)
+            foreach (var type in group.Value)
             {
                 WriteImplementationDetails(type, type.Name, writer);
             }
@@ -43,59 +169,37 @@ internal class RegisteredImplementationsDiagnosticEntry : IDiagnosticEntry
             writer.WriteEndArray();
         }
 
-        //INFO: Types which are registered as open generics are intentionally not included in the above loop as
-        //they cannot be resolved from the DI container as an open generic type. Rather than attempting to dynamically
-        //create a closed type, which could be error-prone, we'll explicitly list the open generic types we know about.
-        writer.WriteStartArray("OpenGenericTypes");
-        WriteImplementationDetails<ICache<string>>(nameof(ICache<string>), writer);
-        WriteImplementationDetails<IConcurrencyLock<string>>(nameof(IConcurrencyLock<string>), writer);
-        WriteImplementationDetails<IMessageStore<string>>(nameof(IMessageStore<string>), writer);
-        writer.WriteEndArray();
-
         writer.WriteEndObject();
 
         return Task.CompletedTask;
     }
-
-    private void WriteImplementationDetails<T>(string serviceName, Utf8JsonWriter writer) where T : class => WriteImplementationDetails(typeof(T), serviceName, writer);
 
     private void WriteImplementationDetails(Type targetType, string serviceName, Utf8JsonWriter writer)
     {
         writer.WriteStartObject();
         writer.WriteStartArray(serviceName);
 
-        try
+        var services = serviceCollectionAccessor.ServiceCollection.Where(descriptor =>
+            descriptor.ServiceType == targetType &&
+            descriptor.ImplementationType != null);
+        if (services.Any())
         {
-            var services = _serviceCollectionAccessor.ServiceCollection.Where(descriptor => descriptor.ServiceType == targetType &&
-                descriptor.ImplementationType != null);
-            if (services.Any())
+            foreach (var service in services)
             {
-                foreach (var service in services)
-                {
-                    var type = service.ImplementationType!;
-                    writer.WriteStartObject();
-                    writer.WriteString("TypeName", type.FullName);
-                    writer.WriteString("Assembly", type.Assembly.GetName().Name);
-                    writer.WriteString("AssemblyVersion", type.Assembly.GetName().Version?.ToString());
-                    writer.WriteEndObject();
-                }
-            }
-            else
-            {
+                var type = service.ImplementationType!;
                 writer.WriteStartObject();
-                writer.WriteString("TypeName", "Not Registered");
-                writer.WriteString("Assembly", "Not Registered");
-                writer.WriteString("AssemblyVersion", "Not Registered");
+                writer.WriteString("TypeName", type.FullName);
+                writer.WriteString("Assembly", type.Assembly.GetName().Name);
+                writer.WriteString("AssemblyVersion", type.Assembly.GetName().Version?.ToString());
                 writer.WriteEndObject();
             }
         }
-        catch (Exception ex)
+        else
         {
-            _logger.LogError(ex, "Error while inspecting registered implementations for {ServiceName}", serviceName);
             writer.WriteStartObject();
-            writer.WriteString("TypeName", "Error resolving service");
-            writer.WriteString("Assembly", "Error resolving service");
-            writer.WriteString("AssemblyVersion", "Error resolving service");
+            writer.WriteString("TypeName", "Not Registered");
+            writer.WriteString("Assembly", "Not Registered");
+            writer.WriteString("AssemblyVersion", "Not Registered");
             writer.WriteEndObject();
         }
 
--- identity-server/test/IdentityServer.UnitTests/Licensing/v2/DiagnosticEntries/RegisteredImplementationsDiagnosticEntryTests.cs
+++ identity-server/test/IdentityServer.UnitTests/Licensing/v2/DiagnosticEntries/RegisteredImplementationsDiagnosticEntryTests.cs
@@ -4,9 +4,7 @@
 using Duende.IdentityServer.Licensing.V2;
 using Duende.IdentityServer.Licensing.V2.Diagnostics.DiagnosticEntries;
 using Duende.IdentityServer.Services;
-using Duende.IdentityServer.Stores;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging.Abstractions;
 using UnitTests.Common;
 
 namespace IdentityServer.UnitTests.Licensing.V2.DiagnosticEntries;
@@ -18,13 +16,13 @@ public class RegisteredImplementationsDiagnosticEntryTests
     {
         var serviceCollection = new ServiceCollection()
             .AddSingleton<IProfileService, MockProfileService>();
-        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(serviceCollection), new NullLogger<RegisteredImplementationsDiagnosticEntry>());
+        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(serviceCollection));
 
         var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
 
         var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
-        var namespaceEntry = registeredImplementations.GetProperty(typeof(IProfileService).Namespace!);
-        var profileServiceEntry = namespaceEntry.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
+        var services = registeredImplementations.GetProperty("Services");
+        var profileServiceEntry = services.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
         var assemblyInfo = profileServiceEntry.GetProperty(nameof(IProfileService)).EnumerateArray().First();
         var expectedTypeInfo = typeof(MockProfileService);
         assemblyInfo.GetProperty("TypeName").GetString().ShouldBe(expectedTypeInfo.FullName);
@@ -33,33 +31,79 @@ public class RegisteredImplementationsDiagnosticEntryTests
     }
 
     [Fact]
-    public async Task WriteAsync_GroupsImplementationsByNamespace()
+    public async Task WriteAsync_GroupsImplementationsByCategory()
     {
-        var serviceCollection = new ServiceCollection()
-            .AddSingleton<IProfileService, MockProfileService>()
-            .AddSingleton<IClientStore, InMemoryClientStore>();
-        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(serviceCollection), new NullLogger<RegisteredImplementationsDiagnosticEntry>());
+        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(new ServiceCollection()));
 
         var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
 
         var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
-        registeredImplementations.TryGetProperty(typeof(IProfileService).Namespace!, out _).ShouldBeTrue();
-        registeredImplementations.TryGetProperty(typeof(IClientStore).Namespace!, out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("Root", out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("Hosting", out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("Infrastructure", out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("ResponseHandling", out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("Services", out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("Stores", out _).ShouldBeTrue();
+        registeredImplementations.TryGetProperty("Validation", out _).ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task WriteAsync_HandlesMultipleRegistrationsForAService()
+    {
+        var serviceCollection = new ServiceCollection()
+            .AddSingleton<IProfileService, DefaultProfileService>()
+            .AddSingleton<IProfileService, MockProfileService>();
+        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(serviceCollection));
+
+        var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
+
+        var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
+        var services = registeredImplementations.GetProperty("Services");
+        var profileServiceEntry = services.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
+        var firstAssemblyInfo = profileServiceEntry.GetProperty(nameof(IProfileService)).EnumerateArray().First();
+        var firstExpectedTypeInfo = typeof(DefaultProfileService);
+        firstAssemblyInfo.GetProperty("TypeName").GetString().ShouldBe(firstExpectedTypeInfo.FullName);
+        firstAssemblyInfo.GetProperty("Assembly").GetString().ShouldBe(firstExpectedTypeInfo.Assembly.GetName().Name);
+        firstAssemblyInfo.GetProperty("AssemblyVersion").GetString().ShouldBe(firstExpectedTypeInfo.Assembly.GetName().Version?.ToString());
+        var secondAssemblyInfo = profileServiceEntry.GetProperty(nameof(IProfileService)).EnumerateArray().Last();
+        var secondExpectedTypeInfo = typeof(MockProfileService);
+        secondAssemblyInfo.GetProperty("TypeName").GetString().ShouldBe(secondExpectedTypeInfo.FullName);
+        secondAssemblyInfo.GetProperty("Assembly").GetString().ShouldBe(secondExpectedTypeInfo.Assembly.GetName().Name);
+        secondAssemblyInfo.GetProperty("AssemblyVersion").GetString().ShouldBe(secondExpectedTypeInfo.Assembly.GetName().Version?.ToString());
     }
 
     [Fact]
     public async Task WriteAsync_HandlesNoServiceRegisteredForInterface()
     {
-        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(new ServiceCollection()), new NullLogger<RegisteredImplementationsDiagnosticEntry>());
+        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(new ServiceCollection()));
 
         var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
 
         var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
-        var namespaceEntry = registeredImplementations.GetProperty(typeof(IProfileService).Namespace!);
-        var profileServiceEntry = namespaceEntry.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
+        var services = registeredImplementations.GetProperty("Services");
+        var profileServiceEntry = services.EnumerateArray().ToList().SingleOrDefault(entry => entry.TryGetProperty(nameof(IProfileService), out _));
         var assemblyInfo = profileServiceEntry.GetProperty(nameof(IProfileService)).EnumerateArray().First();
         assemblyInfo.GetProperty("TypeName").GetString().ShouldBe("Not Registered");
         assemblyInfo.GetProperty("Assembly").GetString().ShouldBe("Not Registered");
         assemblyInfo.GetProperty("AssemblyVersion").GetString().ShouldBe("Not Registered");
+    }
+
+    [Fact]
+    public async Task WriteAsync_ShouldIncludeAllPublicInterfaces()
+    {
+        var interfaces = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetExportedTypes())
+            .Where(type => type.IsInterface && type.IsPublic && type.Namespace != null &&
+                           type.Namespace.StartsWith(
+                               "Duende.IdentityServer"))
+            .Select(type => type.Name);
+        var subject = new RegisteredImplementationsDiagnosticEntry(new ServiceCollectionAccessor(new ServiceCollection()));
+
+        var result = await DiagnosticEntryTestHelper.WriteEntryToJson(subject);
+
+        var registeredImplementations = result.RootElement.GetProperty("RegisteredImplementations");
+        var entries = registeredImplementations.EnumerateObject()
+            .SelectMany(property => property.Value.EnumerateArray()).Select(element => element.EnumerateObject().First().Name);
+        entries.ShouldBe(interfaces, ignoreOrder: true);
     }
 }
