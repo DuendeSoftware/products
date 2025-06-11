4918c587045988294602202197d25bf506544a62
Pieter Germishuys <pieter@pieterg.com>
2025-06-11 15:03:35 +0200

    Add BenchmarkDotNet config to not add units to the output

--- bff/performance/Bff.Benchmarks/BenchmarkBase.cs
+++ bff/performance/Bff.Benchmarks/BenchmarkBase.cs
@@ -2,11 +2,32 @@
 // See LICENSE in the project root for license information.
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Reports;
+using Perfolizer.Metrology;
 
 namespace Bff.Benchmarks;
 
 [ShortRunJob]
+[Config(typeof(Config))]
 public class BenchmarkBase
 {
+}
 
+public class Config : ManualConfig
+{
+    public Config()
+    {
+        var exporter = new CsvExporter(
+            CsvSeparator.CurrentCulture,
+            new SummaryStyle(
+                cultureInfo: System.Globalization.CultureInfo.CurrentCulture,
+                printUnitsInHeader: false,
+                printUnitsInContent: false,
+                timeUnit: Perfolizer.Horology.TimeUnit.Microsecond,
+                sizeUnit: SizeUnit.KB
+            ));
+        AddExporter(exporter);
+    }
 }
