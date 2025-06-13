885dd8fc2c53016f11068ada78a9bab42cda0a76
Erwin van der Valk <erwin@vandervalk.pro>
2025-06-13 16:15:20 +0200

    shorter writing of benchmark setup

--- bff/performance/Bff.Benchmarks/BenchmarkBase.cs
+++ bff/performance/Bff.Benchmarks/BenchmarkBase.cs
@@ -2,11 +2,37 @@
 // See LICENSE in the project root for license information.
 
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+using Perfolizer.Horology;
+using Perfolizer.Metrology;
 
 namespace Bff.Benchmarks;
 
-[ShortRunJob]
+[Config(typeof(BenchmarkConfig))]
 public class BenchmarkBase
 {
 
+}
+
+public class BenchmarkConfig : ManualConfig
+{
+    public BenchmarkConfig()
+    {
+        var exporter = new CsvExporter(
+            CsvSeparator.CurrentCulture,
+            new SummaryStyle(
+                cultureInfo: System.Globalization.CultureInfo.InvariantCulture,
+                printUnitsInHeader: false,
+                printUnitsInContent: false,
+                timeUnit: TimeUnit.Microsecond,
+                sizeUnit: SizeUnit.KB
+            ));
+
+        AddJob(Job.ShortRun);
+
+        AddExporter(exporter);
+    }
 }
--- bff/performance/Bff.Benchmarks/Program.cs
+++ bff/performance/Bff.Benchmarks/Program.cs
@@ -1,10 +1,8 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 
-public class Program
-{
-    static void Main(string[] args) => BenchmarkRunner.Run(typeof(Program).Assembly);
-}
+BenchmarkRunner.Run(typeof(Program).Assembly, ManualConfig.CreateMinimumViable());
