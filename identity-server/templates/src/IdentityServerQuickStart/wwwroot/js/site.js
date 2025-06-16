81bca2146e952acb6b34b208e2a415810d045370
Stu Frankish <stuart.frankish@duendesoftware.com>
2025-06-16 09:27:47 +0100

    Add new IdentityServer QuickStart template

---
+++ identity-server/templates/src/IdentityServerQuickStart/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+import Tags from "../lib/bootstrap5-tags/tags.js";
+
+$(function() {
+  $('[data-confirm]').on('click', function() {
+    return confirm($(this).data('confirm'));
+  });
+
+  $('form :input').on('change', function () {
+    $(this).closest('form').attr('data-changed', 'true');
+    console.log('changed')
+  });
+
+  $('[data-unsaved]').on('click', function () {
+    let changed = $(this).closest('form').attr('data-changed');
+    let message = $(this).data('unsaved');
+    if (changed) {
+      return confirm(message);
+    }
+    return true;
+  });
+
+  Tags.init();
+});
+
