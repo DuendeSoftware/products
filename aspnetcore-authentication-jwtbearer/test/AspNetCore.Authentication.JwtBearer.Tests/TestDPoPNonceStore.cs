955be431900edf7551c19894797d1d4b2221d962
Brett Hazen <2651260+bhazen@users.noreply.github.com>
2025-07-25 15:26:09 -0500

    Use latest ATM packages in JwtBearer extensions tests
    # Conflicts:
    #	Directory.Packages.props

--- aspnetcore-authentication-jwtbearer/test/AspNetCore.Authentication.JwtBearer.Tests/TestDPoPNonceStore.cs
+++ aspnetcore-authentication-jwtbearer/test/AspNetCore.Authentication.JwtBearer.Tests/TestDPoPNonceStore.cs
@@ -1,16 +1,16 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-using Duende.AccessTokenManagement;
+using Duende.AccessTokenManagement.DPoP;
 
 namespace Duende.AspNetCore.Authentication.JwtBearer;
 
 public class TestDPoPNonceStore : IDPoPNonceStore
 {
-    private string _nonce = string.Empty;
-    public Task<string?> GetNonceAsync(DPoPNonceContext context, CancellationToken cancellationToken = new()) => Task.FromResult<string?>(_nonce);
+    private DPoPNonce _nonce = DPoPNonce.Parse(string.Empty);
+    public Task<DPoPNonce?> GetNonceAsync(DPoPNonceContext context, CancellationToken cancellationToken = new()) => Task.FromResult<DPoPNonce?>(_nonce);
 
-    public Task StoreNonceAsync(DPoPNonceContext context, string nonce, CancellationToken cancellationToken = new())
+    public Task StoreNonceAsync(DPoPNonceContext context, DPoPNonce nonce, CancellationToken cancellationToken = new())
     {
         _nonce = nonce;
         return Task.CompletedTask;
