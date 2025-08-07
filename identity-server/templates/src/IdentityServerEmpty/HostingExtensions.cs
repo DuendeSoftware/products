61a35e280fc3976b1c1f73dd013fef589fd46efe
Joe DeCock <josephdecock@gmail.com>
2025-08-07 09:43:23 -0500

    Default development diagnostic config
    - Write diagnostics to files only in development
    - Use large (10mb) chunks in development

--- identity-server/templates/src/IdentityServerEmpty/HostingExtensions.cs
+++ identity-server/templates/src/IdentityServerEmpty/HostingExtensions.cs
@@ -1,9 +1,45 @@
+using System.Globalization;
 using Serilog;
+using Serilog.Filters;
 
 namespace IdentityServerEmpty;
 
 internal static class HostingExtensions
 {
+    public static WebApplicationBuilder ConfigureLogging(this WebApplicationBuilder builder)
+    {
+        // Set up logging to write regular entries to console, and diagnostics data to a file.
+        // See https://docs.duendesoftware.com/identityserver/diagnostics/data
+        builder.Host.UseSerilog((ctx, lc) =>
+        {
+            lc.WriteTo.Logger(consoleLogger =>
+            {
+                consoleLogger.WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                    formatProvider: CultureInfo.InvariantCulture);
+                if (builder.Environment.IsDevelopment())
+                {
+                    consoleLogger.Filter.ByExcluding(Matching.FromSource("Duende.IdentityServer.Diagnostics.Summary"));
+                }
+            });
+            if (builder.Environment.IsDevelopment())
+            {
+                lc.WriteTo.Logger(fileLogger =>
+                {
+                    fileLogger
+                        .WriteTo.File("./diagnostics/diagnostic.log", rollingInterval: RollingInterval.Day,
+                            fileSizeLimitBytes: 1024 * 1024 * 10, // 10 MB
+                            rollOnFileSizeLimit: true,
+                            outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                            formatProvider: CultureInfo.InvariantCulture)
+                        .Filter
+                        .ByIncludingOnly(Matching.FromSource("Duende.IdentityServer.Diagnostics.Summary"));
+                }).Enrich.FromLogContext().ReadFrom.Configuration(ctx.Configuration);
+            }
+        });
+        return builder;
+    }
+
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
         // uncomment if you want to add a UI
