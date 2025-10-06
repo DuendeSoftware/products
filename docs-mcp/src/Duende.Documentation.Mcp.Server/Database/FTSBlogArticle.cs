8ba7f427c609cca41e06de1c301f28017d38f07e
Maarten Balliauw <maarten@maartenballiauw.be>
2025-10-06 11:16:21 +0200

    Documentation MCP server - Initial commit

---
+++ docs-mcp/src/Duende.Documentation.Mcp.Server/Database/FTSBlogArticle.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Duende.Documentation.Mcp.Server.Database;
+
+public class FTSBlogArticle
+{
+    [Key]
+    public required string Id { get; init; }
+    public required string Title { get; init; }
+    public required string Content { get; init; }
+}
