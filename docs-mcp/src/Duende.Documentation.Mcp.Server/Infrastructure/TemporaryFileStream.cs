601c74a3ec1238c45b931ce60da5ea0ab5c41484
Maarten Balliauw <maarten@maartenballiauw.be>
2025-10-06 11:16:21 +0200

    Documentation MCP server - Initial commit

---
+++ docs-mcp/src/Duende.Documentation.Mcp.Server/Infrastructure/TemporaryFileStream.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+namespace Duende.Documentation.Mcp.Server.Infrastructure;
+
+public class TemporaryFileStream : FileStream
+{
+    public static TemporaryFileStream Create()
+    {
+        var path = Path.Combine(
+            Path.GetTempPath(),
+            Guid.NewGuid() + ".tmp");
+
+        return new TemporaryFileStream(path);
+    }
+
+    public static async Task<TemporaryFileStream> CreateFromAsync(Stream otherStream)
+    {
+        var temporaryFileStream = Create();
+
+        await otherStream.CopyToAsync(temporaryFileStream);
+        temporaryFileStream.Position = 0;
+
+        return temporaryFileStream;
+    }
+
+    private readonly string _path;
+
+    private TemporaryFileStream(string path)
+        : base(path, FileMode.OpenOrCreate, FileAccess.ReadWrite) => _path = path;
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        try
+        {
+            File.Delete(_path);
+        }
+        catch
+        {
+            // Best-effort...
+        }
+    }
+}
