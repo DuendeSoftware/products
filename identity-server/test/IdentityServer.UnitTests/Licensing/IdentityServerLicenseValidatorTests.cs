759e9a0a780a708137283efad45284469ef9a875
Erwin van der Valk <erwin@vandervalk.pro>
2025-12-04 08:54:57 +0100

    adjust nullable

--- identity-server/test/IdentityServer.UnitTests/Licensing/IdentityServerLicenseValidatorTests.cs
+++ identity-server/test/IdentityServer.UnitTests/Licensing/IdentityServerLicenseValidatorTests.cs
@@ -81,7 +81,7 @@ public class IdentityServerLicenseValidatorTests(ITestOutputHelper output)
 
         public bool IsEnabled(LogLevel logLevel) => true;
 
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception, string> formatter) => LogMessages.Add(formatter(state, exception!));
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) => LogMessages.Add(formatter(state, exception));
     }
 
     public class MockLoggerProvider(MockLogger logger) : ILoggerProvider
