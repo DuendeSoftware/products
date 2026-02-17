05cf168353bf5f7e5234a80f20ca31853a9f5306
Damian Hickey <dhickey@gmail.com>
2026-02-17 15:31:42 +0100

    Restore accidentally deleted AddCorsPolicyService extension method

--- identity-server/src/IdentityServer/Configuration/DependencyInjection/BuilderExtensions/Additional.cs
+++ identity-server/src/IdentityServer/Configuration/DependencyInjection/BuilderExtensions/Additional.cs
@@ -196,7 +196,18 @@ public static class IdentityServerBuilderExtensionsAdditional
         return builder;
     }
 
-
+    /// <summary>
+    /// Adds a CORS policy service.
+    /// </summary>
+    /// <typeparam name="T">The type of the concrete CORS policy service that is registered in DI.</typeparam>
+    /// <param name="builder">The builder.</param>
+    /// <returns></returns>
+    public static IIdentityServerBuilder AddCorsPolicyService<T>(this IIdentityServerBuilder builder)
+        where T : class, ICorsPolicyService
+    {
+        builder.Services.AddTransient<ICorsPolicyService, T>();
+        return builder;
+    }
 
     /// <summary>
     /// Adds a CORS policy service cache.
