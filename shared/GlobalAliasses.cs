7f04aba92713d53b36985c70e83c8b191888cca7
Damian Hickey <dhickey@gmail.com>
2026-02-20 10:14:49 +0100

    Introduce shared GlobalAliasses.cs with CT = CancellationToken alias repo-wide
    Replace the per-project GlobalAliasses.cs files in BFF with a single
    canonical file in shared/, linked into all projects via src.props,
    test.props, and hosts_and_clients.props.

--- shared/GlobalAliasses.cs
+++ shared/GlobalAliasses.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
+#pragma warning disable IDE0005 // Remove unnecessary using directives
 global using CT = System.Threading.CancellationToken;
-
+#pragma warning restore IDE0005
