30806497470e88d67baed8af24b5935e505c6d2c
Brett Hazen <bhazen@users.noreply.github.com>
2026-02-24 09:43:55 -0600

    Dotnet format

--- identity-server/hosts/Main10/IdentityServerExtensions.cs
+++ identity-server/hosts/Main10/IdentityServerExtensions.cs
@@ -1,12 +1,12 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-using Duende.IdentityServer.Hosts.Shared.Configuration;
 using System.Security.Cryptography.X509Certificates;
 using Duende.IdentityModel;
 using Duende.IdentityServer;
 using Duende.IdentityServer.Configuration;
 using Duende.IdentityServer.Configuration.RequestProcessing;
+using Duende.IdentityServer.Hosts.Shared.Configuration;
 using Duende.IdentityServer.Hosts.Shared.Customization;
 using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.IdentityModel.Tokens;
